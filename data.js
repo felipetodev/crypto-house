42445a9d2bda0ddb725cfbb2d91ef1f24629d84e
felipetodev <fe.ossandon.u@gmail.com>
2021-11-20 16:34:53 -0300

    feat(mock): checkout mock

---
+++ data.js
@@ -0,0 +1,45 @@
+export const mock = [
+  {
+    name: "Alto Polkura",
+    description: "196 - 246 m² útiles",
+    id: "house1",
+    pricing_type: 'fixed_price',
+    local_price: {
+      amount: 9.99,
+      currency: 'USD',
+    },
+    metadata: {
+      user: 'felipeto123'
+    }
+  },
+  {
+    name: "Jardines de la Reserva",
+    description: "341 - 366 m² útiles",
+    id: "house2",
+    pricing_type: 'fixed_price',
+    local_price: {
+      amount: 9.99,
+      currency: 'USD',
+    },
+  },
+  {
+    name: "Terrazas de Casanova",
+    description: "129 m² útiles",
+    id: "house3",
+    pricing_type: 'fixed_price',
+    local_price: {
+      amount: 9.99,
+      currency: 'USD',
+    },
+  },
+  {
+    name: "Viñas de Chicureo",
+    description: "158 - 172 m² útiles",
+    id: "house4",
+    pricing_type: 'fixed_price',
+    local_price: {
+      amount: 9.99,
+      currency: 'USD',
+    },
+  },
+]
