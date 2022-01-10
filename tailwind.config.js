b75b8652682e5b7057ce45eaaab5b2f2fef3d28e
felipetodev <fe.ossandon.u@gmail.com>
2022-01-09 23:05:26 -0300

    Add tailwind to project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-pattern': 'linear-gradient(0deg,#fff 20%,hsla(0,0%,100%,0) 80%)'
+      }
+    }
+  },
+  plugins: []
+}
