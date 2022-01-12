76cca3bcd8efee4eb51481907df37a9b510759e5
felipetodev <fe.ossandon.u@gmail.com>
2022-01-11 23:53:24 -0300

    Add priority load for LCP

--- ui/Image.jsx
+++ ui/Image.jsx
@@ -1,17 +1,23 @@
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import NextImage from 'next/image'
 
-const Image = ({ className, ...restOfProps }) => {
+const Image = ({ className, priority, ...restOfProps }) => {
   const [isLoading, toggleLoading] = useState(true)
 
   const blurDown = isLoading
     ? 'grayscale blur-md'
     : 'grayscale-0 blur-none'
 
+  const isPriority = useMemo(() => {
+    return priority
+      ? { priority }
+      : { loading: 'lazy' }
+  }, [priority])
+
   return (
     <NextImage
       {...restOfProps}
-      loading='lazy'
+      {...isPriority}
       className={`transition-all duration-500 motion-reduce:transition-none motion-reduce:transform-none ${blurDown} ${className}`}
       onLoadingComplete={() => toggleLoading(false)}
     />
