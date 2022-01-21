06e27c0590365067128721db56d0bb8a5379d8da
felipetodev <fe.ossandon.u@gmail.com>
2022-01-21 20:22:23 -0300

    Fix window undefined

--- components/GoogleMaps.jsx
+++ components/GoogleMaps.jsx
@@ -8,23 +8,25 @@ export function GoogleMaps ({ center = DEFAULT_LOCATION, zoom = 18 }) {
   const [map, setMap] = useState(null)
 
   useEffect(() => {
-    setTimeout(() => {
-      if (mapRef.current && !map) {
-        setMap(new window.google.maps.Map(mapRef.current, {
-          center,
-          zoom,
-          mapId: '8e0a97af9386fef'
-        }))
-      }
-      return new window.google.maps.Marker({
-        position: center,
-        map
-      })
-    }, 1000)
+    if (window !== 'undefined' && window.google) {
+      setTimeout(() => {
+        if (mapRef.current && !map) {
+          setMap(new window.google.maps.Map(mapRef.current, {
+            center,
+            zoom,
+            mapId: '8e0a97af9386fef'
+          }))
+        }
+        return new window.google.maps.Marker({
+          position: center,
+          map
+        })
+      }, 1000)
+    }
   }, [mapRef, map])
 
   return (
-    <section className={`${!map ? 'border' : ''}`}>
+    <section className={`${!map ? 'border' : ''} h-[500px]`}>
       <h2 className='my-6 text-2xl font-medium text-gray-700'>
         Peñalolen, Region Metropolitana, Santiago.
         <p className='text-xs text-gray-500 mt-2'>Dirección aproximada otorgada por el anunciante</p>
